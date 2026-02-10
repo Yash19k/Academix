10e403d1657c3e1f21e6669adeee92b48df10788
Yash19k <kshatriyayash19@gmail.com>
2026-02-10 23:13:39 +0530

    commit

--- db.py
+++ db.py
@@ -1,9 +1,12 @@
+import os
 import mysql.connector
 
 def get_db_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="student_db"
+        host=os.environ.get("DB_HOST"),
+        port=int(os.environ.get("DB_PORT")),
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASSWORD"),
+        database=os.environ.get("DB_NAME"),
+        ssl_disabled=False
     )
